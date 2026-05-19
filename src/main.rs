391bf47ece09ab4a74fffc7b8ffc37774eebba42
Dan Taylor <anotheradventure2@gmail.com>
2026-05-19 18:52:35 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,48 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
 
-        command = command.trim().to_string();
-        if command == "exit" {
-            break;
-        } else if command.starts_with("echo ") {
-            let (_, args) = command.split_once(" ").unwrap();
-            println!("{}", args)
+        let command = input.trim();
+        if !command.contains(" ") {
+            // commands with no args
+            match command {
+                "exit" => break,
+                _ => println!("{}: command not found", command.trim()),
+            }
         } else {
-            println!("{}: command not found", command.trim());
+            // commands with args
+            let v: Vec<&str> = command.split(" ").collect();
+
+            match v[0] {
+                "echo" => println!("{}", v[1..v.len()].join(" ")),
+                "type" => {
+                    if ["type", "echo", "exit"].contains(&v[1]) {
+                        println!("{} is a shell builtin", v[1])
+                    } else {
+                        println!("{}: not found", v[1])
+                    }
+                }
+                _ => println!("{}: command not found", command.trim()),
+            }
         }
+
+        // if command == "exit" {
+        //     break;
+        // } else if is_builtin(command) {
+        //     println!("{}", )
+        // } else if command.starts_with("echo ") {
+        //     let (_, args) = command.split_once(" ").unwrap();
+        //     println!("{}", args)
+        // } else {
+        //     println!("{}: command not found", command.trim());
+        // }
     }
 }
+
+// fn is_builtin(command: &str) -> bool {
+//     let foo = ["type", "echo", "exit"]
+//     return ["type", "echo", "exit"].iter().any(|&x| x == command.to_string().trim())
+//     return ["type", "echo", "exit"].contains(&command)
+// }
